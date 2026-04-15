7d80bc5b05fbbf8955739ac7b4abace40a6514a6
HM22-code <83293398+HM22-code@users.noreply.github.com>
2026-04-15 19:14:38 +0200

    Modif du menu pause pour rediriger vers le menu principal

--- menus/pause_menu/pause_menu.gd
+++ menus/pause_menu/pause_menu.gd
@@ -28,7 +28,6 @@ func _on_quit_pressed() -> void:
 	get_tree().paused = false
 	get_tree().quit()
 
-# TODO : si on souhaite faire un bouton pour revenir au menu principal
-# func _on_main_menu_pressed() -> void:
-# 	get_tree().paused = false
-# 	get_tree().change_scene_to_file("res://.../main_menu.tscn")
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://menus/main_menu/main_menu.tscn")
