6ff1a87889f4ab22a914489b212c378a051d4af7
HM22-code <83293398+HM22-code@users.noreply.github.com>
2026-04-21 17:52:01 +0200

    Affichage et mis à jour du score dans les données de jeu lors de collecte de nourriture

--- entities/food/food.gd
+++ entities/food/food.gd
@@ -2,6 +2,7 @@ extends AnimatableBody3D
 
 const AMPLITUDE = 0.2
 const FREQUENCE = 8
+const SCORE_VALUE = 10
 
 var base_y: float
 var time_elapsed: float = 0.0
@@ -17,4 +18,6 @@ func _process(delta: float) -> void:
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
+		if GameData:
+			GameData.add_score(SCORE_VALUE)
 		self.queue_free()
