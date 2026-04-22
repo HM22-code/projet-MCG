455646b95379c52a1d6bce45b771f09d552e4019
David <dludat@students.uni-mainz.de>
2026-04-22 10:04:31 +0200

    Menu options de base

--- menus/main_menu/game_data.gd
+++ menus/main_menu/game_data.gd
@@ -4,6 +4,11 @@ extends Node
 
 const SAVE_PATH = "user://save_game_data.cfg"
 var best_score: int = 0
+var volume_musique: int = 100 #volume musique ambiance
+var volume_effets: int = 100
+var sensibilite: float = 1 #sensibilité du déplacement de la caméra
+#Multiplié avec le déplacement, default = 1
+
 
 func _ready():
 	load_data()
@@ -11,14 +16,36 @@ func _ready():
 func save_data():
 	var config = ConfigFile.new()
 	config.set_value("scores", "best_score", best_score)
+	config.set_value("preferences", "musique_ambiance", volume_musique)
+	config.set_value("preferences", "effets_sonores", volume_effets)
+	config.set_value("preferences", "sensibilite_deplacement", sensibilite)
 	config.save(SAVE_PATH)
 
 func load_data():
 	var config = ConfigFile.new()
 	if config.load(SAVE_PATH) == OK:
 		best_score = config.get_value("scores", "best_score", 0)
+		volume_musique = config.get_value("preferences", "musique_ambiance", 100)
+		volume_effets = config.get_value("preferences", "effets_sonores", 100)
+		sensibilite = config.get_value("preferences", "sensibilite_deplacement", 1)
+		
 
 func submit_score(score: int):
 	if score > best_score:
 		best_score = score
+		save_data()
+		
+func submit_musique_ambiance(volume: int):
+	if (volume != volume_musique and 0 <= volume and volume <= 100):
+		volume_musique = volume
+		save_data()
+
+func submit_effets_sonores(volume: int):
+	if (volume !=  volume_effets and 0 <= volume and volume <= 100):
+		volume_effets = volume_effets
+		save_data()
+		
+func submit_sensibilite_deplacement(sensibilite_nouv: float):
+	if (sensibilite_nouv != sensibilite and 0 < sensibilite_nouv and sensibilite_nouv < 10):
+		sensibilite = sensibilite_nouv
 		save_data()
