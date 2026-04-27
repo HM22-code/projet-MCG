843df38fcd7fde66f08ce91a608310ab5522768b
David <dludat@students.uni-mainz.de>
2026-04-27 17:26:37 +0200

    barre de vie ajouté, points de vie modifiable par fonction

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody3D
 
+const LIFE_POINTS_MAX = 100
+var life_points = 100
 var SPEED = 5.0 #valeurs différents dans classe enfant player_carS
 var JUMP_VELOCITY = 4.5
 
 @onready var score_label: Label = $Camera3D/CanvasLayer/ScoreLabel
-@onready var progress_bar: ProgressBar = $Camera3D/CanvasLayer/LifeBar
+@onready var life_bar: ProgressBar = $Camera3D/CanvasLayer/LifeBar
 
 func _physics_process(delta: float) -> void:
 	# Gravité
@@ -12,6 +14,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	# Mise à jour affichage pour le joueur
 	update_score_label()
+	update_life_bar()
 	# Saut
 	if Input.is_action_just_pressed("saut") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -30,6 +33,32 @@ func _physics_process(delta: float) -> void:
 ## Mise à jour affichage score
 func update_score_label():
 		score_label.text = "Score : " + str(GameData.get_score())
+		
+## Mise à jour affichage barre de vie
+func update_life_bar():
+	if not life_bar:
+		return
+	life_bar.value = life_points #life points doit être modifié et toujours actuel
+	var ratio = get_life_percent()
+	var style = life_bar.get_theme_stylebox("fill").duplicate()
+	if ratio > 0.5:
+		style.bg_color = Color("#8b0000")   # Rouge sang — normal
+	elif ratio > 0.25:
+		style.bg_color = Color("#5a0000")   # Rouge très sombre — danger
+	else:
+		style.bg_color = Color("#2a0000")   # Presque noir — critique		
+	life_bar.add_theme_stylebox_override("fill", style)
+	
+	
+#prendre dégâts d'ennemies
+func take_damage(damage: int):
+	life_points = clamp(life_points - damage, 0, LIFE_POINTS_MAX)
+
+## Retourne le percentage de points de la vie
+func get_life_percent():
+	return life_points / LIFE_POINTS_MAX
+
+
 
 
 # fonctions mouvement caméra à la souris
